02bd727b5487f770036525ab1dc1f6c02d04d651
luuthuong <luuthuong200022000@gmail.com>
2023-04-03 21:35:32 +0700

    Init source

---
+++ App.Backend/App.Backend.API/Extension.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend.API
+{
+    public static class Extension
+    {
+        public static IServiceCollection AddRedisCache(this IServiceCollection service, IConfiguration configuration)
+        {
+            return service.AddStackExchangeRedisCache(redisOptions =>
+            {
+                string connection = configuration.GetConnectionString("Redis");
+                redisOptions.Configuration = connection;
+            });
+        }
+    }
+}
